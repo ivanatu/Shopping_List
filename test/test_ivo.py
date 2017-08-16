2b21e38e38cd0aab7a151a3fe2e4dc29561cec47
ivanatu <ivanatu@gmail.com>
2017-08-16 20:56:56 +0300

    Add files via upload

---
+++ test/test_ivo.py
@@ -0,0 +1,23 @@
+import unittest
+from apptest.ivo import Item
+from apptest.ivo import Cart
+from apptest.ivo import User
+
+class CartTestCases(unittest.TestCase):
+  def setUp(self):
+    self.cart = Cart()
+    
+  def test_cart_property_initialization(self):
+    self.assertIsInstance(self.cart.cartlist, dict, msg='Items is not a dictionary')
+    
+  def test_add_item(self):
+    self.cart.add_item('Eggs', 10)
+    self.assertEqual(self.cart.cartlist['Eggs'], 10)
+    
+  def test_delete_item(self):
+    self.cart.add_item('Oranges', 10)
+    self.cart.add_item('Mangoes', 10)
+    self.cart.delete_item('Oranges', 10)
+    
+    self.assertEqual(self.cart.cartlist['Mangoes'], 10)
+
